b8fb8816774d7f7f55b730a36db65952f2e58817
Weng, Ying Chieh <guiltyxapple@gmail.com>
2023-10-24 16:49:07 +0800

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ## Rather than broute forcing through all pairs
+        ## Use a dict to save seen numbers
+        ## Since the dict is implemented with hash table
+        ## It optimised the search time of "target - nums[i]"
         tb = {}
         for i, num in enumerate(nums):
             if target - num in tb:
