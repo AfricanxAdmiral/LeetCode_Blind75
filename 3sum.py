a1f9586ec7db71b051c7992d8b2292777c018fd4
Weng, Ying Chieh <guiltyxapple@gmail.com>
2023-10-25 16:02:04 +0800

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -3,10 +3,11 @@ class Solution:
         ans, n, i = [], len(nums), 0
         nums = sorted(nums)
         while i < n:
-            # print(f"i: {i}")
+            ## Select the first number
             target = -1 * nums[i]
-            front = i + 1
-            back = n-1
+            
+            ## Search for the second and third number pair
+            front, back = i+1, n-1
             while front < back:
                 s = nums[front] + nums[back]
                 if s < target:
@@ -16,11 +17,14 @@ class Solution:
                 else:
                     tri = [nums[i], nums[front], nums[back]]
                     ans.append(tri)
+                    
+                    ## Skip through the same numbers of the second and third number
                     while front<back and nums[front]==tri[1]:
                         front += 1
                     while front<back and nums[back]==tri[2]:
                         back -= 1
-            
+
+            ## Skip through the same numbers of the first number
             while i+1<n and nums[i+1]==nums[i]:
                 i += 1
             i += 1
